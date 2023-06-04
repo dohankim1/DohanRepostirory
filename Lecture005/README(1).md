c3ab7c7b618c21d7180b63774f4bd6c7814992ad
dohankim1 <yhdh8026@gmail.com>
2023-06-04 15:21:31 +0900

    snakegamelast

--- Lecture005/README(1).md
+++
@@ -1,3 +0,0 @@
-# 유한대학교 VR게임앱학과Programming Pattern Lecutre005:
-* 학번 202227004
-* 이름 김도한
--- Lecture03/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-
-        std::cout<<"누군가 누군가한테 사기를 치다 걸렸다...."<<std::endl;
-        return 0;
-
-}
-
